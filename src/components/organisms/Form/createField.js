5c97a1a78ed9cbc361fd97f714c70403dce00bec
Lucas Daiki <lucas.daiki@sprinklr.com>
2017-02-20 20:52:45 -0300

    Removing obsolete handles

--- src/components/organisms/Form/createField.js
+++ src/components/organisms/Form/createField.js
@@ -1,11 +1,5 @@
 import FIELDS_PROPERTIES from './fieldsProperties';
 
-const handleChange = (updateForm, onChange, key, value, form) => {
-  updateForm(key, value);
-  if (!onChange) return;
-  onChange(form, value);
-};
-
 const createField = (field, updateForm, form) => {
   const fieldProperty = FIELDS_PROPERTIES[field.id];
   const Component = fieldProperty.component;
@@ -15,9 +9,8 @@ const createField = (field, updateForm, form) => {
     id: field.id,
     placeholder: fieldProperty.label,
     validator: fieldProperty.validator,
-    onChange: (key, value) => {
-      handleChange(updateForm, fieldProperty.onChange, key, value, form);
-    }
+    connectedField: fieldProperty.connectedField,
+    onChange: updateForm
   }).component;
 };
 
