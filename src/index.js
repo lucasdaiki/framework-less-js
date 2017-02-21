29efa89a2a1429e963bd1cb7b1c80267e3e908d6
Lucas Daiki <lucas.daiki@sprinklr.com>
2017-02-20 22:56:29 -0300

    Renaming file mock => field

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 require('./scss/base.scss');
 
 import Form from './components/organisms/Form';
-import { fields } from './mock/mock.json';
+import { fields } from './mock/fields.json';
 
 const form = new Form(fields);
-
 document.querySelector('.main').appendChild(form.component);
