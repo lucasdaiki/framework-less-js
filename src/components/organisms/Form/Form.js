a1845e0ca4c8c83559977f7ab2849263502e66f8
Lucas Daiki <lucas.daiki@sprinklr.com>
2017-02-20 21:31:20 -0300

    Improving Form engine

--- src/components/organisms/Form/Form.js
+++ src/components/organisms/Form/Form.js
@@ -4,13 +4,20 @@ import createField from './createField';
 export default class Form {
   constructor (fields) {
     this.data = {};
+    this.fieldElements = [];
+    this.fields = fields;
     this.component = document.createElement('form');
     this.component.className = 'form';
 
     const title = new Title({ text: 'Form' });
     this.component.appendChild(title);
 
-    fields.forEach(field => this.component.appendChild(createField(field, this.handleChange.bind(this), this.component)));
+    fields.forEach(field => {
+      const fieldElement = createField(field, this.handleChange.bind(this), this.component);
+      this.fieldElements.push(fieldElement);
+    });
+
+    this.fieldElements.forEach(field => this.component.appendChild(field));
 
     return this;
   }
