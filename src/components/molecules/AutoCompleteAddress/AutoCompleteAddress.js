dd18d58794c010a9bb9e51065039b0dec7cde002
Lucas Daiki <lucas.daiki@sprinklr.com>
2017-02-23 21:04:23 -0300

    Testing AutoCompleteAddress

--- src/components/molecules/AutoCompleteAddress/AutoCompleteAddress.js
+++ src/components/molecules/AutoCompleteAddress/AutoCompleteAddress.js
@@ -4,15 +4,16 @@ import Input from '../../atoms/Input';
 require('./AutoCompleteAddress.scss');
 
 export default class AutoCompleteAddress extends Input {
-  constructor (properties) {
+  constructor (properties, Adapter = GoogleAdapter) {
     super(properties);
+    this.Adapter = Adapter;
 
     const input = this.component.querySelector('input');
     const map = document.createElement('div');
     map.classList.add('map__container');
     this.component.appendChild(map);
 
-    const googleData = new GoogleAdapter(input, map);
+    const googleData = new this.Adapter(input, map);
     googleData.input.placeholder = '';
     googleData.input.addEventListener('change', this.handleChange.bind(this));
 
---
+++ src/components/molecules/AutoCompleteAddress/AutoCompleteAddress.spec.js
@@ -0,0 +1,30 @@
+import sinon from 'sinon';
+import AutoCompleteAddress from './AutoCompleteAddress';
+
+
+const addEventListener = sinon.spy();
+const onChange = sinon.spy();
+
+
+class Adapter {
+  constructor () {
+    this.input = { addEventListener };
+  }
+}
+
+describe('#AutoCompleteAddress', () => {
+  test('should render', () => {
+    const input = new AutoCompleteAddress({ id: 'AutoCompleteAddress' }, Adapter);
+    expect(input.component).toBeDefined();
+    expect(addEventListener.called).toBeTruthy();
+  });
+
+  describe('#handleChange', () => {
+    test('should render', () => {
+      const input = new AutoCompleteAddress({ id: 'AutoCompleteAddress', onChange }, Adapter);
+      input.handleChange();
+      expect(input.value).toEqual({});
+      expect(onChange.called).toBeTruthy();
+    });
+  });
+});
